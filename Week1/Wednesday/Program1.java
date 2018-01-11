8f7520fc29de16082572614b1f15d42ef9e41dc0
Alvaro Tzul <tzulalvaro@gmail.com>
2018-01-10 17:55:44 -0800

    Create Program1.java
    This is my method

---
+++ Week1/Wednesday/Program1.java
@@ -0,0 +1,33 @@
+public program1{
+
+  public static void main(String []args)
+  {
+      //Make at program prints a right triangel
+      
+      /**
+      Output:
+      
+      *
+      **
+      ***
+      ****
+      *****
+      */
+  }
+  public static triangleOne(){ // Alvaro's method
+  
+      for(int i = 0; i< 5; i++)
+      {
+          
+          for(int j = 0; j< i;j++)
+          {
+             System.out.print("*");
+          }
+          System.out.println();
+  }
+
+
+
+
+
+}
