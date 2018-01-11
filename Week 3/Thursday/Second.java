348f2e2a613e8dc2772796dbe13db2a3b07bbd84
Esther Tapia <estherdt44@gmail.com>
2018-01-11 14:59:23 -0800

    Esther method
    week 3, thursday, method

--- Week 3/Thursday/Second.java
+++ Week 3/Thursday/Second.java
@@ -17,6 +17,26 @@ public class Second{
       
       
   }
+  
+  public static void sumPosNeg(int[] a)   //Esther's method
+	{
+		int sumP = 0;
+		int sumN = 0;
+		for(int i = 0; i < a.length; i++)
+		{
+			if(a[i] > 0)
+			{
+				sumP = sumP + a[i];
+			}
+			else
+			{
+				sumN = sumN + a[i];
+			}
+		}
+		
+		System.out.println("Positive: " + sumP);
+		System.out.println("Negative: " + sumN);
+	}
 
 
 
