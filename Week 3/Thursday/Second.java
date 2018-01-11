e8a0a51526769718a6a66465762604a267485eea
Alvaro Tzul <tzulalvaro@gmail.com>
2018-01-11 14:48:22 -0800

    Create Second.java

---
+++ Week 3/Thursday/Second.java
@@ -0,0 +1,23 @@
+public class Second{
+  
+  public static void main(String []args)
+  {
+      // Make a method that accepts an int array and 
+      //Print 2 sums : one for postives and one for negatives
+      //
+      //output:
+      //positive: ##
+      //negative: ##
+      int [] array = {2,4,6,-5,7,2,-5,6,10,-35,66};
+      
+  }
+  public static void (int [] array)
+  {
+      
+      
+      
+  }
+
+
+
+}
