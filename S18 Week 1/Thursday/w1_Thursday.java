fe8f4558c0db6ac17bc79d4dbb1af39b34ec471b
Alvaro Tzul <tzulalvaro@gmail.com>
2018-02-01 15:27:26 -0800

    Update w1_Thursday.java

--- S18 Week 1/Thursday/w1_Thursday.java
+++ S18 Week 1/Thursday/w1_Thursday.java
@@ -1,12 +1,29 @@
+public class Thursday{
 
-public static String abbreviate(String word) { //Kevin's Method
+ public static void main(String [] args){
+  // Method that accepts a string that consists of one word.
+  // and returns an abbreivation of it which consists of the first letter,
+  // the number of letters in between the first and the last, and the last letter.
+  // 
+  // Example: 
+  //       " Hello"
+         
+  // Output: H3o
+         
+         
+         
+         
+ }
+
+
+ public static String abbreviate(String word) { //Kevin's Method
         char[] charWord = word.toCharArray();
         StringBuilder abbWord = new StringBuilder();
         abbWord.append(charWord[0] + ((charWord.length-2) + "") + charWord[charWord.length-1]);
         return abbWord.toString();
     }
 
-public static String newWord(String n)  // Daniel's Method
+ public static String newWord(String n)  // Daniel's Method
     {
         int num = n.length() - 2;
         char a = n.charAt(0);
@@ -15,24 +32,4 @@ public static String newWord(String n)  // Daniel's Method
         
         return temp;
     }
-public static StringBuilder convertString(String scs){ //Rushabh's Method
-		int i=scs.length()-2;
-		char c=scs.charAt(0);
-		char d=scs.charAt(scs.length()-1);
-		StringBuilder sc=new StringBuilder();
-		sc.append(c);
-		sc.append(i);
-		sc.append(d);
-		System.out.print(sc);
-		return sc;
-	}
-
-static String work(String input){ //juancarlos's method
-		int length = input.length();
-		
-		String work = input.charAt(0) + String.valueOf(length-2) + input.charAt(length-1);
-		return work;
-		
-	}
-
-
+}
