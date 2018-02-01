5a6f0ad9984416c8046935eef6a452c204eb9aab
darkhall9 <kevinramirez266@yahoo.com>
2018-02-01 15:15:02 -0800

    Kevin's Method

--- S18 Week 1/Thursday/w1_Thursday.java
+++ S18 Week 1/Thursday/w1_Thursday.java
@@ -1,1 +1,7 @@
 
+public static String abbreviate(String word) { //Kevin's Method
+        char[] charWord = word.toCharArray();
+        StringBuilder abbWord = new StringBuilder();
+        abbWord.append(charWord[0] + ((charWord.length-2) + "") + charWord[charWord.length-1]);
+        return abbWord.toString();
+    }
