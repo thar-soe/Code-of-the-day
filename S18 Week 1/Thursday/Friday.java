a108e24b6e9cace5d2bb84ee0642acb50bbf7a78
Alvaro Tzul <tzulalvaro@gmail.com>
2018-02-02 02:10:52 -0800

    Update Friday.java

--- S18 Week 1/Thursday/Friday.java
+++ S18 Week 1/Thursday/Friday.java
@@ -2,9 +2,36 @@ public class Friday{
 
 
   public static void main(String [] args){
+      /**
+         * 
+         * Make a method that accepts a string 
+         * that consists of multiple of words and 
+         * only abbreviates the words that have a 
+         * length of 4 or greater.
+         * 
+         * 
+         * But if the string has only one word of length 3 or less
+         * just return the string.
+         * 
+         * example:
+         * 
+         * "Hello World and to the mass universe"
+        //    5     5    3   2  3    4     8     // note that words of length
+        *                                        // 3 or less are not abbreviated
+         * 
+         * output: 
+         * 
+         *  "H3o W3d and to the m2s u6e"
+         * 
+        
+        */
   
   
-  
+  }
+  public static String AlvaroMethod(String str){
+    
+    
+    
   }
 
 }
