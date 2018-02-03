b6f4ef42476b3dd5aa6a39e426dadb7f3d7107e8
Alvaro Tzul <tzulalvaro@gmail.com>
2018-02-03 01:09:17 -0800

    Create first.java

---
+++ Week1/Thursday/first.java
@@ -0,0 +1,22 @@
+public class first{
+
+
+  
+   public static void main(String [] args){
+   // Make a function that accepts a number and that number will determine the length of the triangle.
+   
+   
+   
+   }
+   
+   
+   public static void AlvaroMethdo(int length){
+   
+   
+   
+   
+   }
+
+
+
+}
