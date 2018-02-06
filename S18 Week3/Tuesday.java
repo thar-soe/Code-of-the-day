927a6d7a19e02ebe9cf683ec90117fdc41229507
Alvaro Tzul <tzulalvaro@gmail.com>
2018-02-06 14:44:37 -0800

    Create Tuesday.java

---
+++ S18 Week3/Tuesday.java
@@ -0,0 +1,19 @@
+publci class{
+  
+  public static void main(String [] args){
+  
+          
+//        Complete the function caffeineBuzz, which takes a non-zero integer as it's one argument.
+//
+//If the integer is divisible by 3, return the string "Java".
+//
+//If the integer is divisible by 3 and divisible by 4, return the string "Coffee"
+//
+//If the integer is one of the above and is even, add "Script" to the end of the string.
+//
+//Otherwise, return the string "mocha_missing!"
+  
+  }
+
+
+}
